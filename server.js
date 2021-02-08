84f79871990b46c9cf5404ff658674fbb7688fcb
Brandon Craig <thegamingraven13@gmail.com>
2021-02-07 19:00:47 -0800

    Created a routes folder with apiRoutes and htmlRoutes js files. server.js requires these files.

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
