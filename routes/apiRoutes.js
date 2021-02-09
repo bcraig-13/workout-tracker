12c7ff3b8c4da2efc38b05a05c0db65c23f874e5
Brandon Craig <thegamingraven13@gmail.com>
2021-02-09 15:57:37 -0800

    Added api routes for workouts and workouts/:id. Also added api/workouts/range so charts display correctly.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 
 module.exports = function (app) {
-  app.get("api/workouts", (req, res) => {
+  app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
       .then((workout) => {
         res.json(workout);
@@ -10,8 +10,31 @@ module.exports = function (app) {
         res.json(err);
       });
   });
-  //I think this is right??
+
   app.post("/api/workouts", (req, res) => {
+    db.Workout.create({}).then((response) => {
       res.json(response);
-    })
+    });
+  });
+
+  app.put("/api/workouts/:id", ({ params, body }, res) => {
+    db.Workout.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { exercises: body } },
+      { upsert: true, useFindAndModify: false },
+      (updatedWorkout) => {
+        res.json(updatedWorkout);
+      }
+    );
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then((workout) => {
+        res.json(workout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 };
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const mongoose = require ("mongoose");
-
 const PORT = process.env.PORT || 3000;
-const db = require("./models");
 const app = express();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/3000',
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
