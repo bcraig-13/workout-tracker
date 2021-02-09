859eea9d6e52ca955aa9c582cf79f68a4ba4f269
Brandon Craig <thegamingraven13@gmail.com>
2021-02-09 15:15:44 -0800

    Added res.sendfile to html routes.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,34 +1,15 @@
-const db = require("../models");
+const path= require("path");
 
 module.exports = function(app) {
     app.get("/", (req, res) => {
-        if (req) {
-            return res.redirect("index");
-            //display last workout?
-        }
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     })
 
     app.get("/exercise", (req, res) => {
-        if (req) {
-            return res.redirect("exercise");
-        }
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     })
 
     app.get("/stats", (req, res) => {
-        if (req) {
-            //needs db.{dbname}.findAll() to display stats?
-            return res.redirect("stats");
-        }
-        //Need something else for all three of the above app.gets
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
     })
 }
-
-// const router = require("express").Router();
-// const path= require("path");
-// router.get("/exercise", function (req, res) {
-//    res.sendFile(path.join(__dirname, "../public/exercise.html"));   
-// });
-// router.get("/stats", function (req, res) {
-//       res.sendFile(path.join(__dirname, "../public/stats.html"));   
-//    });
-// module.exports = router;
